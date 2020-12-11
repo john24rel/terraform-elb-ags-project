2d4ef81c3fcb2fbaaceed76b42a80417a3fffef1
EC2 Default User <john oshikoya>
2020-12-11 22:02:48 +0000

    latest

---
+++ lc.tf
@@ -0,0 +1,12 @@
+resource "aws_launch_template" "example" {
+  name_prefix          = "example"
+  image_id             = "${data.aws_ami.ubuntu.id}"
+  instance_type        = "${var.instance_type}"
+  instance_market_options {
+    market_type = "spot"
+  }
+  security_group_names = ["${aws_security_group.asg-sec-group.name}"]
+  key_name             = "${aws_key_pair.asg-key-pair.key_name}"
+  user_data = "${base64encode(data.template_file.init.rendered)}"
+}
+ 
---
+++ test/asg_module.tf
@@ -0,0 +1,9 @@
+module "webapp" {
+  source           = "../"
+  region           = "us-east-1"
+  max_size         = "1"
+  min_size         = "1"
+  desired_capacity = "1"
+  image_owner      = "137112412989"
+  instance_type    = "c5.large"
+}
